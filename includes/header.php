1f7ca89b7fd4d16fcb6d7568df2138b716830018
flaviengrs <flaviengrs@gmail.com>
2022-10-04 10:04:59 +0200

    header + css

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,37 @@ include 'classes/DB.php';
 session_start();
 
 DB::connect();
+
+echo '
+<!DOCTYPE html> 
+<html lang="fr">
+<script src="js/header.js"> </script>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Exemple d\'un menu responsive</title>  
+		<link rel="stylesheet" href="../css/header.css">  
+	</head> 
+	<body>   
+		<div class="topnav" id="myTopnav">
+			<a href="#home" class="active">Acceuil</a>
+			<a href="#news">Trouver des producteurs</a>
+			<a href="#contact">Contact</a>
+			<a href="#about">About</a>
+			<a href="javascript:void(0);" class="icon" onclick="openMenu()">
+				<i class="fa fa-bars"><label>☰</label></i>
+			</a>
+		</div>
+	</body>  
+</html>  
+';
+
+
+
+
+
+
+
 $query = DB::query("
 	SELECT *
 	FROM users
---
+++ includes/js/header.js
@@ -0,0 +1,12 @@
+
+/* 
+Ouvre le menu de navigation pour mobile
+*/
+function openMenu() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+	  x.className += " responsive";
+	} else {
+	  x.className = "topnav";
+	}
+  } 
