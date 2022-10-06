462d6bc29f6b5541017ac4a34607f223abd0df9a
remy385 <remy.gaboriau@gmail.com>
2022-10-06 09:44:02 +0200

    update profile

--- page/profil.php
+++ page/profil.php
@@ -8,6 +8,7 @@ echo '<div class="content">
 <br>';
 if($tab["description"]!==""){
     echo '<textarea id="description" name="description" row="10" col="50" maxlength="500">'.$tab["description"].'</textarea>';
+    $_SESSION["description"] = $tab["description"];
 }
 else{
     echo '<textarea id="description" name="description" row="10" col="50" maxlength="500"></textarea>';
@@ -17,6 +18,7 @@ echo'   <br>
 <br>';
 if($tab["horraire_lundi"]!==""){
         echo '<textarea id="horairelun" name="horairelun" row="1" col="50" maxlength="50" placeholder="8h-17h ou fermé" style="resize:none">'.$tab["horraire_lundi"].'</textarea>';
+        $_SESSION["horraire_lundi"] = $tab["horraire_lundi"];
     }
 else{
     echo '<textarea id="horairelun" name="horairelun" row="1" col="50" maxlength="50" placeholder="8h-17h ou fermé" style="resize:none"></textarea>';
@@ -26,6 +28,7 @@ echo'<br>
 <br>';
 if($tab["horraire_mardi"]!==""){
         echo '<textarea id="horairemar" name="horairemar" row="1" col="50" maxlength="50" placeholder="8h-17h ou fermé" style="resize:none">'.$tab["horraire_mardi"].'</textarea>';
+        $_SESSION["horraire_mardi"] = $tab["horraire_mardi"];
     }
 else{
     echo '<textarea id="horairemar" name="horairemar" row="1" col="50" maxlength="50" placeholder="8h-17h ou fermé" style="resize:none"></textarea>';
@@ -35,6 +38,7 @@ echo '<br>
 <br>';
 if($tab["horraire_mercredi"]!==""){
         echo '<textarea id="horairemer" name="horairemer" row="1" col="50" maxlength="50" placeholder="8h-17h ou fermé" style="resize:none">'.$tab["horraire_mercredi"].'</textarea>';
+        $_SESSION["horraire_mercredi"] = $tab["horraire_mercredi"];
     }
 else {
     echo '<textarea id="horairemer" name="horairemer" row="1" col="50" maxlength="50" placeholder="8h-17h ou fermé" style="resize:none"></textarea>';
@@ -44,6 +48,7 @@ echo '<br>
 <br>';
 if($tab["horraire_jeudi"]!==""){
         echo '<textarea id="horairejeu" name="horairejeu" row="1" col="50" maxlength="50" placeholder="8h-17h ou fermé" style="resize:none">'.$tab["horraire_jeudi"].'</textarea>';
+        $_SESSION["horraire_jeudi"] = $tab["horraire_jeudi"];
     }
 else {
     echo '<textarea id="horairejeu" name="horairejeu" row="1" col="50" maxlength="50" placeholder="8h-17h ou fermé" style="resize:none"></textarea>';
@@ -53,6 +58,7 @@ echo '<br>
 <br>';
 if($tab["horraire_vendredi"]!==""){
         echo '<textarea id="horaireven" name="horaireven" row="1" col="50" maxlength="50" placeholder="8h-17h ou fermé" style="resize:none">'.$tab["horraire_vendredi"].'</textarea>';
+        $_SESSION["horraire_vendredi"] = $tab["horraire_vendredi"];
     }
 else{
     echo '<textarea id="horaireven" name="horaireven" row="1" col="50" maxlength="50" placeholder="8h-17h ou fermé" style="resize:none"></textarea>';
